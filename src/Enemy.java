de795301ae5a266da71e172f8445530b7bd5f6a3
miles <gaviganmiles@gmail.com>
2026-03-23 14:54:27 -0700

    commit .5
    Co-authored-by: Landon-J2 <Landon-J2@users.noreply.github.com>

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,24 +1,28 @@
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 
-public class Player extends GameObject {
-
+public class Enemy extends GameObject {
+    private Gun activeGun;
     private int speed = 200;
     
-    public Player(double x, double y){
+    public Enemy(double x, double y){
         super(x, y, 50, 50, "assets\\whale.png");
     }
 
     @Override
     public void move(double deltaTime){
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
             setX(getX() - (speed * deltaTime));
-        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+        } else if (Gdx.input.isKeyPressed(Input.Keys.D)){
             setX(getX() + (speed * deltaTime));
-        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+        } else if (Gdx.input.isKeyPressed(Input.Keys.W)){
             setY(getY() + 25);
         }
 
+    }
+
+    public void shootGun(){
+        activeGun.shoot();
     }
 
 }
---
+++ src/Gun.java
@@ -0,0 +1,13 @@
+public class Gun{
+    private int magSize;
+    private int shotDelay;
+
+    public Gun(int magSize, int shotDelay){
+        this.magSize = magSize;
+        this.shotDelay = shotDelay;
+    }
+
+    public void shoot(){
+        
+    }
+}
